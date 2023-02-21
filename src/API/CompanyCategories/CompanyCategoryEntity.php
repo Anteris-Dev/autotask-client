f08b18358ac235cd21bec88a48116619c7585bbe
pssi <simon.psenner@iteas.at>
2023-02-21 16:05:08 +0100

    fixed property spelling

--- src/API/CompanyCategories/CompanyCategoryEntity.php
+++ src/API/CompanyCategories/CompanyCategoryEntity.php
@@ -10,7 +10,7 @@ use Spatie\DataTransferObject\DataTransferObject;
  */
 class CompanyCategoryEntity extends DataTransferObject
 {
-    public int $displayColorRgb;
+    public int $displayColorRGB;
     public $id;
     public bool $isActive;
     public ?bool $isApiOnly;
